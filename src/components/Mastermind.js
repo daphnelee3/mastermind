93eb452b456f225d6d63372a53876cc2b435a59e
Daphne Lee <daphnelee92@gmail.com>
2020-02-08 13:11:17 -0500

    correct combo parsed

--- src/components/Mastermind.js
+++ src/components/Mastermind.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-class Mastermind extends Component {
+export default class Mastermind extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            winningCombination: [],
+            winningCombination: '',
             attempts: 0,
             previousAttempts: [],
             status: '', //win, lose, playing
         }
+    }
+
+    async componentDidMount() {
+        try {
+            const { data } = await axios.get('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new');
+            const numbers = data.split('\n')
+            numbers.pop()
+            const integers = numbers.map(num => parseInt(num))
+            this.setState({ winningCombination: integers });
+        } catch {
+            console.log('error generating number')
+        }
+    }
+
+    render() {
+        const { winningCombination, attempts, previousAttempts, status } = this.state
+        console.log(typeof winningCombination, winningCombination)
+        return (
+            <div>
+                <div >{winningCombination} correct combo test</div>
+            </div>
+        )
     }
 }
