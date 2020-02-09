d24644bc3807f96505f96b1062d5264e0171c913
Daphne Lee <daphnelee92@gmail.com>
2020-02-09 14:30:41 -0500

    handleSubmit win lose statuses & feedback

--- src/components/Mastermind.js
+++ src/components/Mastermind.js
@@ -10,9 +10,10 @@ export default class Mastermind extends Component {
             currentGuess: [null, null, null, null],
             attempts: 10,
             previousAttempts: [],
-            status: '', //win, lose, playing
+            status: 'playing', //won, lost
+            invalidGuess: false
         }
-        this.handleChange = this.handleChange.bind(this);
+        this.handleGuess = this.handleGuess.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -21,36 +22,97 @@ export default class Mastermind extends Component {
             const { data } = await axios.get('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new');
             const numbers = data.split('\n')
             numbers.pop()
-            const integers = numbers.map(num => parseInt(num))
+            const integers = numbers.map(num => parseInt(num)) //parsed into an array of nums
             this.setState({ winningCombination: integers });
         } catch {
             console.log('error generating number')
         }
     }
 
-    handleChange(guessIdx, event) {
+    handleGuess(guessIdx, event) {
         const newGuess = this.state.currentGuess.slice()
         newGuess[guessIdx] = parseInt(event.target.value)
         this.setState({ currentGuess: newGuess })
     }
 
-    //if num not selected, prompt to select one
+    //order of submit: won?
+    //vvv separate into multiple methods to call
+    //decrement attempts, currentGuess & msg into previousAttempts, check status, reset currentGuess
+    //push feedback in previousAttempts
     handleSubmit(event) {
+        const { attempts, winningCombination, currentGuess, status, previousAttempts } = this.state;
         event.preventDefault();
-        this.setState({});
+
+        const correctNumandPosition = "Correct number and position"
+        const correctNumOnly = "Correct Number"
+        const noCorrectGuesses = "Incorrect, please try again"
+
+        //invalid selection
+        if (attempts > 1 && (currentGuess.includes(NaN) || currentGuess.includes(null))) {
+            this.setState({ invalidGuess: true })
+            return
+        } else {
+            this.setState({ invalidGuess: false })
+        }
+
+        //checks if won
+        let hasWon = true
+        for (let i = 0; i < winningCombination.length; i++) {
+            const winningNum = winningCombination[i];
+            const playerGuess = currentGuess[i];
+            if (winningNum !== playerGuess) {
+                hasWon = false
+                break;
+            }
+        }
+        if (hasWon === true) {
+            this.setState({ status: 'won' });
+        } else if (attempts === 1) {
+            this.setState({ status: 'lost' });
+        } else {
+            const history = {};
+            let message;
+            for (let i = 0; i < winningCombination.length; i++) {
+                const winningNum = winningCombination[i];
+                const playerGuess = currentGuess[i];
+                if (winningNum === playerGuess) {
+                    message = correctNumandPosition;
+                    break;
+                } else if (winningCombination.includes(playerGuess)) {
+                    message = correctNumOnly;
+                    break;
+                } else {
+                    message = noCorrectGuesses;
+                }
+            }
+            history.feedback = message;
+            history.guess = currentGuess;
+            console.log(history)
+            //decrement attempts remaining and build history
+            this.setState(prevState => {
+                return {
+                    attempts: prevState.attempts - 1,
+                    previousAttempts: [...prevState.previousAttempts, history]
+                }
+            });
+        }
+
+
     }
 
     render() {
-        const { winningCombination, currentGuess, attempts, previousAttempts, status } = this.state
-        console.log(currentGuess)
+        const { winningCombination, currentGuess, attempts, previousAttempts, status, invalidGuess } = this.state
+        console.log(currentGuess, previousAttempts)
         return (
             <div>
                 <div >{winningCombination} correct combo</div>
                 <div >{attempts} attempts remaining</div>
+                {status === 'lost' ? <div>LOSE</div> : null}
                 <PlayerInput
                     currentGuess={currentGuess}
-                    handleChange={this.handleChange}
+                    handleGuess={this.handleGuess}
                     handleSubmit={this.handleSubmit} />
+                {invalidGuess === true ? <div> Invalid Selection. Please Choose 4 Numbers!</div> : ''}
             </div>
         )
     }
--- src/components/PlayerInput.js
+++ src/components/PlayerInput.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { NumberSelectors } from './NumberSelectors'
 
-export const PlayerInput = ({ currentGuess, handleChange, handleSubmit }) => {
+export const PlayerInput = ({ currentGuess, handleGuess, handleSubmit }) => {
     console.log('something CHANGED')
     return (
         <div>
             {currentGuess.map((digit, idx) => (
-                <NumberSelectors key={idx} guessIdx={idx} handleChange={handleChange} />
+                <NumberSelectors key={idx} guessIdx={idx} handleGuess={handleGuess} />
             ))}
             <button className="button" onClick={handleSubmit}>submit</button>
         </div>
