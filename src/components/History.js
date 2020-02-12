08b9ae10e84609b23da68ac8f65b3417980693eb
Daphne Lee <daphnelee92@gmail.com>
2020-02-12 11:26:34 -0500

    brain image

--- src/components/History.js
+++ src/components/History.js
@@ -11,7 +11,7 @@ const History = () => {
                     After rejections by several leading toy makers, the rights were obtained by a small British firm, Invicta Plastics Ltd. The firm originally manufactured the game itself, but later licensed its manufacture to Hasbro in most of the world.
                     Mastermind is actually just a readaptation of an all-time’s classic game called “Bulls and Cows” or “Cows and Bulls” in English and “Numerello” in Italian. It is also reported on a few web pages that in the late 1960s Ken Thompson of Bell Labs and J. M. Grochow of MIT wrote programs that played “Moo” (a version of Bulls and Cows) on Unix and Multics operating systems, respectively.
                     Nevertheless, after over 50 million copies sold in around 80 countries, Mastermind board game is still on the store shelves today.
-            </p>
+                </p>
             </main>
         </div>
     );
