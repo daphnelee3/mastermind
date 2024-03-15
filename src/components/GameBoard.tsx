1210b4466bb58f02ccca07a26b672fbfb310feb8
Daphne Lee <daphnelee92@gmail.com>
2024-03-15 16:15:52 -0400

    build game board

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,28 @@
+import { useGameContext } from '../GameContext';
+import getColorClass from '../getColorClass';
+import { Color } from '../types';
+
+type GameBoardProps = { onFeedbackSubmit: () => void };
+
+const GameBoard = ({ onFeedbackSubmit }: GameBoardProps) => {
+  const { state } = useGameContext();
+
+  return (
+    <div className="bg-white p-8 shadow-lg rounded-lg">
+      {state.rows.map((row, rowIdx) => (
+        <div key={rowIdx} className={`${rowIdx === state.activeRowIdx ? 'bg-gray-100' : ''}`}>
+          <div className="flex justify-center">
+            {row.guess.map((color, circleIdx) => (
+              <div
+                key={circleIdx}
+                className={`w-10 h-10 rounded-full m-2 ${color ? getColorClass(color as Color) : 'bg-gray-300'}`}
+              ></div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GameBoard;
---
+++ src/getColorClass.ts
@@ -0,0 +1,22 @@
+import { Color } from './types';
+
+const getColorClass = (color: Color): string => {
+  switch (color) {
+    case 'red':
+      return 'bg-red-500';
+    case 'green':
+      return 'bg-green-500';
+    case 'blue':
+      return 'bg-blue-500';
+    case 'yellow':
+      return 'bg-yellow-500';
+    case 'orange':
+      return 'bg-orange-500';
+    case 'purple':
+      return 'bg-purple-500';
+    default:
+      return 'bg-gray-300';
+  }
+};
+
+export default getColorClass;
